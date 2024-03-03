2d645b600a892fe6f12cfbfe736ebd3e7401ba26
olufsenglo <olsheaolufsen@gmail.com>
2024-03-03 21:19:51 +0100

    Add Header

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import logo from "../assets/images/logo.svg";
+import { BiShoppingBag } from "react-icons/bi"
+import { HiOutlineMenuAlt1 } from "react-icons/hi"
+import { NavLink, Link } from "react-router-dom";
+
+function Header() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <header>
+        <div className="container flex-b">
+          <div className="logo">
+            <Link to="/">
+              <img src={logo} alt="JoeysCoin Logo" />
+            </Link>
+          </div>
+          <nav className={open ? "mobile-view" : "desktop-view"}>
+            <ul className="flex-b-g">
+              <li onClick={() => setOpen(null)}>
+                <NavLink className="active" to="/">Home</NavLink>
+              </li>
+              <li onClick={() => setOpen(null)}>
+                <NavLink className="" to="/crypto">Crypto</NavLink>
+              </li>
+              <li onClick={() => setOpen(null)}>
+                <NavLink className="" to="/portfolio">Porfolio</NavLink>
+              </li>
+              <li onClick={() => setOpen(null)}>
+                <NavLink className="" to="/shop">shop</NavLink>
+              </li>
+              <li onClick={() => setOpen(null)}>
+                <NavLink className="" to="/blog">blog</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div class="account flex-b-g">
+            <button>
+                <BiShoppingBag size={22}/>
+            </button>
+            <Link to="/dasboard" style={{display: "flex"}}>
+              Dashboard
+            </Link>
+            <button class="open-menu" onClick={() => setOpen(!open)}>
+                <HiOutlineMenuAlt1 />
+            </button>
+          </div>
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
