e5d0483460b7bd7338b7e88568b6b3288bfc56c8
olufsenglo <olsheaolufsen@gmail.com>
2024-03-03 20:22:05 +0100

    Install React router dom

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { HomePage } from './components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
